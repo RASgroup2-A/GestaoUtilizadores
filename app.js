fbc8e94ac7b19d20e4e546d601ec0f6b33b96fae
João Pedro Gonçalves <jpagoncalves7@gmail.com>
2023-11-27 18:23:41 +0000

    Conexao a base de dados realizada com sucesso

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
+//var logger = require('morgan');
 
 // Conexão à BD
 var mongoose = require('mongoose');
@@ -9,10 +9,10 @@ if (process.env.MONGODB_URL){
     var mongoDB = process.env.MONGODB_URL;
 }
 else{
-    var mongoDB = 'mongodb://localhost:27017/GestaoUsers';
+    var mongoDB = 'mongodb://localhost:27017/test';
 }
 
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(mongoDB);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error...'));
 db.on('open', function() { console.log("Conexão ao MongoDB realizada com sucesso...") })
@@ -21,7 +21,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({limit: '50mb', extended: true  }));
 
